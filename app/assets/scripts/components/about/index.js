45faa7cfa7850171a4f58262fa7428f92152156c
Daniel da Silva <danielfdsilva@gmail.com>
2020-11-07 11:35:03 +0000

    Add about placeholder

--- app/assets/scripts/components/about/index.js
+++ app/assets/scripts/components/about/index.js
@@ -10,7 +10,14 @@ class About extends Component {
       <App pageTitle='About'>
         <Constrainer>
           <Prose>
-            About targetting
+            <img
+              src='/assets/graphics/layout/targetting_colored.svg'
+              alt='Targetting logotype'
+              width='96'
+              height='96'
+            />
+            <p>Targetting is an archery scoring application.</p>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
           </Prose>
         </Constrainer>
       </App>
